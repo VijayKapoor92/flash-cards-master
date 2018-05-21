d67789ecde868e3908f08d2547707bf9b6c74d45
VijayKapoor92 <vijayk_1@hotmail.com>
2018-05-21 19:47:06 -0300

    feat: Cria arquivo de cores

---
+++ utils/colors.js
@@ -0,0 +1,11 @@
+export const
+    black = '#000',
+    gray = '#757575',
+    white = '#fff',
+    green = '#047200',
+    red = '#ca0e0c',
+    charcoal = '#333',
+    halfGray = '#666',
+    lightGray = '#ccc',
+    blue = '#0337ca'
+;
