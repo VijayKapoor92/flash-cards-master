7e30ae184611ae86d70e96c5412bcad4d59f617e
VijayKapoor92 <vijayk_1@hotmail.com>
2018-05-21 19:48:26 -0300

    feat: Cria arquivo do estado inicialda aplicacao

---
+++ utils/initialState.js
@@ -0,0 +1,35 @@
+export const
+    initialState = {
+        React: {
+            title: 'React',
+            questions: [
+                {
+                    question: 'What is React?',
+                    answer: 'A JavaScript library for building user interfaces'
+                },
+                {
+                    question: 'When was React released?',
+                    answer: 'March 2013'
+                }
+            ]
+        },
+        Redux: {
+            title: 'Redux',
+            questions: [
+                {
+                    question: 'What is redux?',
+                    answer: 'A predictable state container for JavaScript apps'
+                }
+            ]
+        },
+        JavaScript: {
+            title: 'JavaScript',
+            questions: [
+                {
+                    question: 'What is a closure?',
+                    answer: 'The combination of a function and the lexical environment within which that function was declared.'
+                }
+            ]
+        }
+    };
+
